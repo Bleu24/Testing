e6dba38954038a6f161421bd71a17c536ba17393
Bryan Reamillo <bryanreamillo2403@gmail.com>
2026-03-19 16:03:20 +0800

    feat: implement string manipulation functions and Caesar cipher

---
+++ src/main.js
@@ -0,0 +1,84 @@
+const capitalize = (str) => {
+    if (str.includes(" ")) {
+        const result = [];
+        str.split(" ").forEach(word => {
+            result.push(word[0].toUpperCase() + word.slice(1));
+        });
+
+        return result.join(" ");
+    }
+
+    const strArr = str.split("");
+    return strArr[0].toUpperCase() + strArr.slice(1).join("");
+};
+
+const reverseString = (str) => {
+    if (!str) return;
+    return str.split("").reverse().join("");
+};
+
+const calculator = {
+    add(addend1, addend2) {
+        return addend1 + addend2;
+    },
+
+    subtract(min, sub) {
+        return min - sub;
+    },
+
+    divide(dividend, divisor) {
+        return dividend / divisor;
+    },
+
+    multiply(multiplicand, multiplier) {
+        return multiplicand * multiplier;
+    }
+
+
+};
+
+// helper functions
+const createCipherMap = (alphabet, cipherText) => {
+    const map = {};
+    [...alphabet].forEach((c, i) => {
+        map[c] = cipherText[i];
+    });
+    return map;
+};
+
+const encrypt = (str, map) => {
+    return [...str].reduce((prev, curr) => {
+        if (/[!,\s+$]/g.test(curr)) return prev + curr;
+
+        if (isCapitalized(curr)) {
+            const key = curr.toLowerCase();
+            return prev + map[key].toUpperCase();
+        }
+
+        return prev + (map[curr] ? map[curr] : curr);
+    }, "");
+};
+
+const shiftAlphabet = (alphabet, shift) => {
+    return alphabet.split("").slice(shift).join("").toLowerCase() + alphabet.split("").slice(0, shift).join("").toLowerCase();
+};
+
+const isCapitalized = (char) => char === char.toUpperCase();
+
+const caesarCipher = (str, shift) => {
+    const alphabet = "abcdefghijklmnopqrstuvwxyz";
+    const cipherMap = createCipherMap(alphabet, shiftAlphabet(alphabet, shift));
+
+    return encrypt(str, cipherMap);
+};
+
+
+export {
+    capitalize,
+    reverseString,
+    calculator,
+    caesarCipher,
+    createCipherMap,
+    shiftAlphabet,
+    isCapitalized
+};
